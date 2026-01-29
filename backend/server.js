fa9be506c893eaa088cd221be302e5bcf8b7e3e3
Houria <houriahasbell@gmail.com>
2026-01-29 22:30:47 +0100

    feat(backend): Add latest server.js file and json packages

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes.js");
+const pdfRoutes = require("./routes/pdfRoutes.js");
+
 
 
 dotenv.config();
@@ -15,6 +16,8 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/pdf", pdfRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
